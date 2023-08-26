06339d0a286d11f49eb1d1b472ab55e37dc6f3cb
fakamda <facundoelcardenal@gmail.com>
2023-08-26 04:09:55 -0300

    a

--- src/Dao/product.factory.js
+++ src/Dao/product.factory.js
@@ -7,6 +7,11 @@ switch (config.persistence) {
         const { default: ProductDAO } = await import('../Dao/product.mongo.dao.js')
         Product = ProductDAO
         break
+    case 'FILE':
+        const { default: ProducFileDAO } = await import('../Dao/product.file.dao.js')
+        Product = ProductFileDAO
+        break
+        
     default:
         break
 }
