886d23a785dcec27a17497984a7c158b9523b510
fakamda <facundoelcardenal@gmail.com>
2023-09-20 01:48:36 -0300

    falta el winston

--- src/app.js
+++ src/app.js
@@ -1,20 +1,23 @@
 import express from 'express'
 import handlebars from 'express-handlebars'
 import { Server } from 'socket.io'
-import viewsRouter from './router/views.routes.js'
-import ProductRouter from './router/product.routes.js'
-import CartRouter from './router/carts.routes.js'
 import mongoose from 'mongoose'
 import passport from 'passport'
 import initializePassport from './config/passport.js'
 import { PORT, SESSION_SECRET_KEY } from './config/config.js'
 import session from "express-session";
 import cookieParser from "cookie-parser";
-import sessionsRouter from './router/sessions.routes.js'
 import MongoConnection from './database.js'
 import { socketServerConnection } from './socketServer.js'
 import { passportCall } from './middlewares/auth.middleware.js'
+
+import viewsRouter from './router/views.routes.js'
+import ProductRouter from './router/product.routes.js'
 import MockRouter from './router/mock.routes.js'
+import LoggerRouter from './router/logger.routes.js'
+import CartRouter from './router/carts.routes.js'
+import sessionsRouter from './router/sessions.routes.js'
+
 
 const app = express()
 
@@ -25,7 +28,7 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(
   session({
-    secret: SESSION_SECRET_KEY, // Cambia esto por una cadena secreta para firmar las cookies
+    secret: SESSION_SECRET_KEY,
     resave: false,
     saveUninitialized: true,
   })
@@ -60,10 +63,10 @@ try {
 
     app.use("/api/products", ProductRouter);
     app.use("/api/carts", passportCall("jwt"), CartRouter);
-    // app.use("/api/carts", CartRouter);
     app.use("/products", passportCall("jwt"), viewsRouter);
     app.use("/session", sessionsRouter)
     app.use("/api/mock", MockRouter)
+    app.use("/logger", LoggerRouter)
 
     socketServerConnection()
 
